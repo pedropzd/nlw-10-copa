6d450d503fe04b9ee151ad11f43b666fbbf52e15
pedropzd <pedropzd@gmail.com>
2022-11-07 10:12:59 -0300

    add new games

--- main.js
+++ main.js
@@ -22,11 +22,17 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
   createCard(
     "28/11",
     "segunda",
-    createGame("switzerland", "13:00", "brazil") +
+    createGame("south-korea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
       createGame("portugal", "16:00", "uruguay")
-  ) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  )
